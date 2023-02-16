18dbf54e0cc6089c058682b82f4d7a2244401f88
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-02-16 09:15:45 +0100

    Updates

---
+++ assets/index-c130d413.js
@@ -0,0 +1,17 @@
+(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const c="/assets/typescript-f6ead1af.svg";function l(r){let o=0;const s=n=>{o=n,r.innerHTML=`count is ${o}`};r.addEventListener("click",()=>s(o+1)),s(0)}document.querySelector("#app").innerHTML=`
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${c}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`;l(document.querySelector("#counter"));const a=async()=>{const o=await(await fetch("https://mobster-backend-production.up.railway.app/pets")).json();console.log({pets:o.data.pets})};a();
